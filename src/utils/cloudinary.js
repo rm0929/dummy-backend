ac51cb13d033cedf170b439f16a7380edb655940
rm0929 <rohanmathur2001@gmail.com>
2024-08-14 16:58:40 -0700

    added middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above on the cloudinary website to copy your API secret
+});
+
+const uploadOnCloudinary = async(localFilePath) => {
+    try {
+        if (!localFilePath) return null
+            //upload hte file in cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+                resource_type: "auto"
+            })
+            // file has been uploaded successfully
+        console.log("file is uploaded on cloudinary", response.url);
+        console.log(response);
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+// cloudinary.v2.uploader.upload(
+//     'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+//         public_id: 'shoes'
+//     },
+//     function(error, result) { console.log(result); }
+// );
+
+export { uploadOnCloudinary }
