9a9147ea628329165237c3be59263bbf3513c443
rm0929 <rohanmathur2001@gmail.com>
2024-08-22 01:45:24 -0700

    Add new routes for user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -336,7 +336,7 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
 })
 
 // update the cover image
-const updateUserCoverimage = asyncHandler(async(req, res) => {
+const updateUserCoverImage = asyncHandler(async(req, res) => {
     const coverImageLocalPath = req.file && req.file.path
 
     if (!coverImageLocalPath) {
@@ -361,6 +361,82 @@ const updateUserCoverimage = asyncHandler(async(req, res) => {
         .status(200)
         .json(
             new ApiResponse(200, user, "Cover image update successfully")
+        )
+})
+
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+    const { username } = req.params
+
+    if (!(username && username.trim())) {
+        throw new ApiError(400, "username is missing")
+    }
+    User.find({ username })
+
+    const channel = await User.aggregate([
+        // pipelines
+        {
+            $match: {
+                username: username && username.toLowerCase()
+
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {
+                            $in: [req.user && req.user._id, "$subscribers.subscriber"]
+                        },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+
+    if (!(channel && channel.length)) {
+        throw new ApiError(404, "Channel does not exists")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, channel[0], "User channel fetched successfully")
         )
 })
 
@@ -422,6 +498,7 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverimage,
+    updateUserCoverImage,
+    getUserChannelProfile,
     getWatchHistory
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
@@ -25,5 +25,19 @@ router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser) //middleware verifyJFT while logout
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/changePassword").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+
+router.route("/c/username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
